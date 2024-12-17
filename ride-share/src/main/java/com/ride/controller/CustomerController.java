497f14e352a7b61b02792ab5a3cf35d7822920a7
Ivanshi garg <90376747+ivanshi06@users.noreply.github.com>
2024-12-17 18:19:49 +0530

    jwt auth added

--- ride-share/src/main/java/com/ride/controller/CustomerController.java
+++ ride-share/src/main/java/com/ride/controller/CustomerController.java
@@ -1,6 +1,10 @@
 package com.ride.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -26,6 +30,15 @@ public class CustomerController {
 	@GetMapping("/{id}")
 	public Customer findById(@PathVariable(name = "id") int customerId) {
 		return  customerService.getById(customerId);
+	}
+
+	@GetMapping("/me")
+	public ResponseEntity<Customer> authenticatedCustomer(){
+		Authentication authentication= SecurityContextHolder.getContext().getAuthentication();
+		UserDetails currentCustomer=(UserDetails) authentication.getPrincipal();
+		System.out.println(currentCustomer);
+		Customer customer=customerService.findByUsername(currentCustomer.getUsername());
+		return ResponseEntity.ok(customer);
 	}
 	
 }
--- ride-share/src/main/java/com/ride/service/CustomerService.java
+++ ride-share/src/main/java/com/ride/service/CustomerService.java
@@ -1,17 +1,45 @@
 package com.ride.service;
 
+import java.time.Instant;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.UUID;
+
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
+import com.ride.dto.LoginCustomerDto;
+import com.ride.dto.RegisterCustomerDto;
+import com.ride.dto.UpdateCustomerDto;
 import com.ride.model.Customer;
 import com.ride.repository.ICabRepo;
 import com.ride.repository.ICustomerRepo;
 
 @Service
 public class CustomerService {
+	private final ICustomerRepo customerRepository;
+	private final AuthenticationManager authenticationManager;
 	
-	@Autowired
-	private ICustomerRepo customerRepository;
+	private final PasswordEncoder passwordEncoder;
+	
+	public CustomerService(
+		ICustomerRepo customerRespository,
+		AuthenticationManager authenticationManager,
+		PasswordEncoder passwordEncoder
+		
+) {
+		this.customerRepository= customerRespository;
+		this.authenticationManager= authenticationManager;
+		this.passwordEncoder= passwordEncoder;
+	}
+	
+	public Customer findByUsername(String username) {
+        return customerRepository.findByEmail(username)
+                .orElseThrow(() -> new RuntimeException("User not found with username: " + username));
+    }
 	
 	public Customer add(Customer user) {
 		return customerRepository.saveAndFlush(user);
@@ -25,5 +53,60 @@ public class CustomerService {
 		return customerRepository.findById(id).get();
 	}
 	
+	
 
-}
+	public Customer signup(RegisterCustomerDto input) {
+		Customer user=new Customer();
+    	long timestamp = Instant.now().toEpochMilli();
+    	System.out.println(passwordEncoder.encode(input.getPassword()));
+    	user.setEmail(input.getEmail());
+    	user.setName(input.getName());
+    	user.setPhone(input.getPhone());
+    	user.setPassword(passwordEncoder.encode(input.getPassword()));
+
+//		String verificationToken = UUID.randomUUID().toString();
+//		user.setVerificationToken(verificationToken);
+//		System.out.println(verificationToken);
+//		String verificationLink = "http://localhost:8085/users/verify-email?token=" + verificationToken;
+//		emailClient.sendEmail(new EmailRequest(
+//				user.getEmail(),
+//				"Click the link to verify your email: " + verificationLink,
+//				"Verify Your Email Address"
+//		));
+        return customerRepository.save(user);
+    }
+	
+	public Customer authenticate(LoginCustomerDto input) {
+    	Customer user = customerRepository.findByEmail(input.getEmail())
+                .orElseThrow(() -> new RuntimeException("User not found"));
+//    	Collection<GrantedAuthority> authorities = mapRolesToAuthorities(List.of(user.getRoles()));
+//    	System.out.println(authorities);
+    	System.out.println("---------------------");
+    	 authenticationManager.authenticate(
+                new UsernamePasswordAuthenticationToken(
+                        input.getEmail(),
+                        input.getPassword()
+                )
+        );
+        return customerRepository.findByEmail(input.getEmail())
+                .orElseThrow();
+    }
+    public List<Customer> allUsers() {
+        List<Customer> users = new ArrayList();
+        customerRepository.findAll().forEach(users::add);
+        customerRepository.findAll().forEach(System.out::println);
+        return users;
+    }
+    
+    
+    public Customer updateUser(String email, UpdateCustomerDto newUser) {
+    	Customer user = customerRepository.findByEmail(email)
+                .orElseThrow(() -> new RuntimeException("User not found"));
+    	user.setName(newUser.getName());
+    	user.setName(newUser.getName());
+    	user.setPhone(newUser.getPhone());
+//    	user.setProfileImg(newUser.getProfileImgUrl());
+    	return customerRepository.save(user);
+    }
+	}
+	
